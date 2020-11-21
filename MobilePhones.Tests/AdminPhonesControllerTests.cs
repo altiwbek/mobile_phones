cfc4b604d9e2f5aafdfd418da7da4c3644be398b
Altynbek <altiwbek@gmail.com>
2020-11-21 20:03:21 +0600

    Add Create Phone Test

--- MobilePhones.Tests/AdminPhonesControllerTests.cs
+++ MobilePhones.Tests/AdminPhonesControllerTests.cs
@@ -11,47 +11,50 @@ using MobilePhones.Models;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Threading;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MobilePhones.Tests
 {
-    public class AdminPhonesControllerTests
+    public class AdminPhonesControllerTests : CleanUpAfterTests
     {
-        [Fact]
-        public async Task TestCreate()
+        private PhonesController _controller;
+        private MobileContext _context;
+        private Mock<IWebHostEnvironment> _hostEnv;
+        public AdminPhonesControllerTests()
         {
-            var hostEnv = new Mock<IWebHostEnvironment>();
-
-           
-
             var options = new DbContextOptionsBuilder<MobileContext>()
                              .UseInMemoryDatabase(databaseName: "schooldb")
                              .Options;
-            using (var context = new MobileContext(options))
+            _context = new MobileContext(options);
+            _hostEnv = new Mock<IWebHostEnvironment>();
+            _controller = new PhonesController(_context, _hostEnv.Object);
+        }
+
+        [Fact]
+        public async Task TestCreate()
+        {
+            cleanUpDir = @"..\..\..\..\MobilePhones.Tests\testfiles\images\phone_images\";
+            cleanUpSearchPattern = "*.jpg";
+            int beforeCount = _context.Phones.Count();
+            var file = new Mock<IFormFile>();
+            var sourceImg = File.OpenRead(@"..\..\..\..\MobilePhones.Tests\testfiles\phone_test_image.jpg");
+            var fileName = "test.jpg";
+            file.Setup(f => f.FileName).Returns(fileName).Verifiable();
+            _hostEnv.Setup(h => h.WebRootPath).Returns(@"..\..\..\..\MobilePhones.Tests\testfiles\").Verifiable();
+            var phone = new Phone()
             {
-                var controller = new PhonesController(context, hostEnv.Object);
+                Name = "My Phone",
+                Company = "Company",
+                Price = 444,
+                ImageFile = file.Object
+            };
 
-                var file = new Mock<IFormFile>();
-                var sourceImg = File.OpenRead(@"..\..\..\..\MobilePhones.Tests\testfiles\phone_test_image.jpg");
-               
-                var fileName = "QQ.jpg";
-
-                file.Setup(f => f.FileName).Returns(fileName).Verifiable();
-              
-
-                hostEnv.Setup(h => h.WebRootPath).Returns(@"..\..\..\..\MobilePhones.Tests\testfiles\").Verifiable();
-                
-                var phone = new Phone()
-                {
-                    Name = "My Phone",
-                    Company = "Comapny",
-                    Price = 444,
-                    ImageFile = file.Object
-                };
-
-                var result = await controller.Create(phone);
-
-                Assert.Contains(fileName, phone.ImageName);
-            }
+            var result = await _controller.Create(phone);
+            Assert.Contains(fileName, phone.ImageName);
+            Assert.Equal(beforeCount + 1, _context.Phones.Count());
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
         }
     }
 }
---
+++ MobilePhones.Tests/CleanUpAfterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MobilePhones.Tests
+{
+    public abstract class CleanUpAfterTests : IDisposable
+    {
+        protected string cleanUpDir;
+        protected string cleanUpSearchPattern;
+        public void Dispose()
+        {
+            if(cleanUpDir != null && cleanUpSearchPattern != null)
+            {
+                string[] cleanUpFiles = Directory.GetFiles(cleanUpDir, cleanUpSearchPattern);
+                foreach (var ff in cleanUpFiles)
+                {
+                    File.Delete(ff);
+                }
+            }
+           
+        }
+    }
+}
